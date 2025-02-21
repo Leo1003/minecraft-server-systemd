44c931786781597613dada9b6963397ec5513065
Leo Chen <leo881003@gmail.com>
2025-02-21 20:12:23 +0800

    make rcon.sh automatically read from server.properties

--- instance_scripts/rcon.sh
+++ instance_scripts/rcon.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
-export MCRCON_HOST=localhost
-export MCRCON_PORT=25575
-export MCRCON_PASS=
+SERVER_PROPERTIES_PATH="${SERVER_PROPERTIES_PATH:-server.properties}"
+if [ -z "$MCRCON_HOST" ]; then
+    export MCRCON_HOST=localhost
+fi
+if [ -z "$MCRCON_PORT" ]; then
+    if [ -r "$SERVER_PROPERTIES_PATH" ]; then
+        export MCRCON_PORT=`grep -e '^rcon\.port=' "$SERVER_PROPERTIES_PATH" | cut -d '=' -f 2-`
+    else
+        echo "Warning: Couldn't read rcon.port from $SERVER_PROPERTIES_PATH" >&2
+    fi
+fi
+if [ -z "$MCRCON_PASS" ]; then
+    if [ -r "$SERVER_PROPERTIES_PATH" ]; then
+        export MCRCON_PASS=`grep -e '^rcon\.password=' "$SERVER_PROPERTIES_PATH" | cut -d '=' -f 2-`
+    else
+        echo "Warning: Couldn't read rcon.password from $SERVER_PROPERTIES_PATH" >&2
+    fi
+fi
+
 mcrcon "$@"
 
